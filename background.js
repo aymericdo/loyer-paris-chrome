954de94c52f189ac3a3b3385ea09c2be06c4d646
aymeric dominique <aymeric.dominique@gmail.com>
2023-02-01 23:17:30 +0100

    chrome

--- background.js
+++ background.js
@@ -8,10 +8,32 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-      if (tabs.length) {
-        chrome.tabs.sendMessage(tabs[0].id, { message: "urlHasChanged" });
-      }
-    });
+    if (isAvailableUrl(tab.url)) {
+      chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        if (tabs.length) {
+          chrome.tabs.sendMessage(tabs[0].id, { message: "urlHasChanged" });
+        }
+      });
+    }
   }
 });
+
+function isAvailableUrl(url) {
+  if (!url) return null;
+
+  return ['seloger.com',
+    'leboncoin.fr',
+    'jinka.fr',
+    'pap.fr',
+    'logic-immo.com',
+    'immobilier.lefigaro.fr',
+    'facebook.com/marketplace',
+    'orpi.com',
+    'gensdeconfiance.com',
+    'bellesdemeures.com',
+    'lux-residence.com',
+    'bienici.com',
+    'fnaim.fr',
+    'superimmo.com',
+    'locservice.fr'].some((u) => url.includes(u));
+}
